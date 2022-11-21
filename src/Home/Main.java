f7b5ee48682e89fcbdfc7f5d6cc8088f460ea120
TheHaterNoob <HATERNOOBLOL@gmail.com>
2022-11-21 12:58:43 -0600

    Carga 2

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Logininfo logininfo= new Logininfo(); 
         Login login = new Login();
-        
+        System.out.println("Test");
     }
     
 }
